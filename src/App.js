fb9e5736a2714eae4be7c25642ec76053fda6b8c
mostafa <mostafahp44@gmail.com>
2022-12-28 15:07:29 +0200

    'ok'

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ListTask from './components/ListTasks';
+
+function App() {
+  return (
+    <div className="App">
+    <ListTask/>
+    </div>
+  );
+}
+
+export default App;
